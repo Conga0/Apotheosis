ce1d7bcb6e9294518f0f9959038c11711ac3614c
Conga Lyne <43751663+Conga0@users.noreply.github.com>
2023-06-22 00:23:35 +0930

    Hotfix2
    Goodnight

--- files/scripts/magic/markerportals/portal_use_blue.lua
+++ files/scripts/magic/markerportals/portal_use_blue.lua
@@ -15,7 +15,7 @@ function portaliumCheck(plyr_id, from_x, from_y, to_x, to_y)
                         local target_x, target_y = (to_x + (x - from_x))
                         local target_y = (to_y + (y - from_y))
 
-                        local did_hit r_x, r_y = Raytrace(to_x,to_y,target_x,target_y)
+                        local did_hit, r_x, r_y = Raytrace(to_x,to_y,target_x,target_y)
                         if did_hit then
                             --Move all creatures over to the new location if a solid wall was in the way
                             EntitySetTransform(v,r_x, r_y)
