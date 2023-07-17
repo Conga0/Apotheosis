fc5df1b700304a3fffbe7f8e9f1d82609a15e66f
Spoop <minebloxgc@gmail.com>
2023-07-17 15:47:56 -0400

    Beams, Devourers, and Twitchy Individuals
    Made Esoteric Being's beam attack look more nice, Gave Esoteric Being an emissive, Made the Corrupt Twitchy sprite, and Improved the Planar Magic Devourer

--- files/scripts/newgame/elemental_test_01_biome_map.lua
+++ files/scripts/newgame/elemental_test_01_biome_map.lua
@@ -5,4 +5,4 @@ local w = 102
 local h = 48
 
 BiomeMapSetSize(w, h)
-BiomeMapLoadImage(0, 0, "mods/Apotheosis/files/biome_impl/end_game_02/biome_map.png")
+BiomeMapLoadImage(0, 0, "mods/Apotheosis/files/biome_impl/end_game_02/biome_map_02.png")
