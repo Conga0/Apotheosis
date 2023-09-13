fe58022ab28b4deefc79cae1e48a3c28c5e25868
Conga Lyne <43751663+Conga0@users.noreply.github.com>
2023-09-13 13:28:23 +0930

    Hot Fix
    Fixed a bug which caused liquids spawning in holy moutain basins soft-locking a teleportation related secret

--- data/scripts/biomes/temple_altar_top_shared.lua
+++ data/scripts/biomes/temple_altar_top_shared.lua
@@ -36,19 +36,7 @@ function spawn_altar_top_alt(x, y, is_solid)
 	if( y > 12000 ) then
 		LoadPixelScene( "data/biome_impl/temple/altar_top_boss_arena.png", file_visual, x, y-40, "", true )
 	else
-		if (randomtop == 5) then
-			LoadPixelScene( "data/biome_impl/temple/altar_top_water.png", file_visual, x, y-40, "", true )
-		elseif (randomtop == 8) then
-			LoadPixelScene( "data/biome_impl/temple/altar_top_blood.png", file_visual, x, y-40, "", true )
-		elseif (randomtop == 11) then
-			LoadPixelScene( "data/biome_impl/temple/altar_top_oil.png", file_visual, x, y-40, "", true )
-		elseif (randomtop == 13) then
-			LoadPixelScene( "data/biome_impl/temple/altar_top_radioactive.png", file_visual, x, y-40, "", true )
-		elseif (randomtop == 15) then
-			LoadPixelScene( "data/biome_impl/temple/altar_top_lava.png", file_visual, x, y-40, "", true )
-		else
-			LoadPixelScene( "mods/Apotheosis/files/biome_impl/temple/altar_top_alt.png", file_visual, x, y-40, "", true )
-		end
+		LoadPixelScene( "mods/Apotheosis/files/biome_impl/temple/altar_top_alt.png", file_visual, x, y-40, "", true )
 	end	
 
 	if is_solid then LoadPixelScene( "data/biome_impl/temple/solid.png", "", x, y-40+300, "", true ) end
