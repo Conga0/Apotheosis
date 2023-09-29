7688529cc2449098d7f1d581daba74f910432fef
Nahtan <nathan.titirangi@gmail.com>
2023-09-29 18:20:48 +1300

    remove print

--- files/scripts/misc/trip_red_end.lua
+++ files/scripts/misc/trip_red_end.lua
@@ -1,5 +1,4 @@
 local amount = GlobalsGetValue("conga_red_sand_effect_amount")
-print(amount)
 amount = amount - 0.005
 GameSetPostFxParameter("conga_red_sand_effect_amount", amount, 0, 0, 0)
 GlobalsSetValue("conga_red_sand_effect_amount", amount)
