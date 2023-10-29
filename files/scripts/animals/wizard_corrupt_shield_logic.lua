29e6189c1f39341d296bc4cb70692c443ebf306f
Conga Lyne <43751663+Conga0@users.noreply.github.com>
2023-10-29 19:28:48 +0930

    Shield Hotfix
    Optimised corrupt master shield creation
    Fixed corrupt masters making shields more often than intended

--- files/scripts/animals/wizard_corrupt_shield_logic.lua
+++ files/scripts/animals/wizard_corrupt_shield_logic.lua
@@ -4,12 +4,14 @@ function damage_received( damage, desc, entity_who_caused, is_fatal )
 	local entity_id    = GetUpdatedEntityID()
 	local pos_x, pos_y = EntityGetTransform( entity_id )
 	local frame = GameGetFrameNum()
+	local vcomp = 0
 
 	local colour
 	local comps = EntityGetComponent(entity_id,"VariableStorageComponent")
 	for k=1,#comps
 	do comp = comps[k]
 		if ComponentGetValue2(comp,"name") == "shield_colour" then
+			vcomp = comp
 			colour = ComponentGetValue2(comp,"value_string")
 			last_frame = ComponentGetValue2(comp,"value_int")
 		end
@@ -26,8 +28,8 @@ function damage_received( damage, desc, entity_who_caused, is_fatal )
 		end
 	end
 	
-	local eid = EntityLoad( "mods/apotheosis/files/entities/misc/shields/shield_" .. colour .. ".xml", pos_x, pos_y )
+	local eid = EntityLoad( table.concat({"mods/apotheosis/files/entities/misc/shields/shield_",colour,".xml"}), pos_x, pos_y )
 	EntityAddChild( entity_id, eid )
 
-	ComponentSetValue2(comp,"value_int",GameGetFrameNum())
+	ComponentSetValue2(vcomp,"value_int",GameGetFrameNum())
 end
