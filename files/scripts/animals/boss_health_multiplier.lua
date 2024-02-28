581d31dc1f48d09a743cf501923abdca9fe4439c
Conga Lyne <43751663+Conga0@users.noreply.github.com>
2024-02-29 09:23:20 +0930

    Hotfix
    Fixed instances where Wandsin Arvostelija could cause crashes from loading too many projectiles

--- files/scripts/animals/boss_health_multiplier.lua
+++ files/scripts/animals/boss_health_multiplier.lua
@@ -1,5 +1,3 @@
-dofile_once( "data/scripts/lib/utilities.lua" )
-
 
 local entity_id = GetUpdatedEntityID()
 
@@ -11,9 +9,12 @@ local comp = EntityGetFirstComponent( entity_id, "DamageModelComponent" )
 
 local health = ComponentGetValue2( comp, "hp" )
 local health_max = ComponentGetValue2( comp, "max_hp" )
+local blood_mult = ComponentGetValue2( comp, "blood_multiplier" )
 
 health = health * (healthMultiplier / 100)
 health_max = health_max * (healthMultiplier / 100)
+blood_mult = blood_mult / (healthMultiplier / 100)
 
 ComponentSetValue( comp, "max_hp", tostring(health_max) )
 ComponentSetValue( comp, "hp", tostring(health) )
+ComponentSetValue( comp, "blood_multiplier", tostring(blood_mult) )
--- files/scripts/animals/boss_health_multiplier_half.lua
+++ files/scripts/animals/boss_health_multiplier_half.lua
@@ -1,5 +1,3 @@
-dofile_once( "data/scripts/lib/utilities.lua" )
-
 
 local entity_id = GetUpdatedEntityID()
 
@@ -11,11 +9,14 @@ local comp = EntityGetFirstComponent( entity_id, "DamageModelComponent" )
 
 local health = ComponentGetValue2( comp, "hp" )
 local health_max = ComponentGetValue2( comp, "max_hp" )
+local blood_mult = ComponentGetValue2( comp, "blood_multiplier" )
 
 health = health * (healthMultiplier / 200)
 health_max = health_max * (healthMultiplier / 200)
+blood_mult = blood_mult / (healthMultiplier / 200)
 
 ComponentSetValue( comp, "max_hp", tostring(health_max) )
 ComponentSetValue( comp, "hp", tostring(health) )
+ComponentSetValue( comp, "blood_multiplier", tostring(blood_mult) )
 
 --Conga: Technically this is bugged and reduces enemies to 50% of their intended HP, but it seems to work fine in terms of gameplay balance so, /shrug
