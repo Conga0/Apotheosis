d023c36924859548a42de38547d9650e280da39e
Conga Lyne <43751663+Conga0@users.noreply.github.com>
2024-12-15 15:31:29 +1030

    Volcanic Egg Fix
    Volcanic Eggs now hatch more aggressively
    Fixed Volcanic Eggs to not print debug data

--- files/scripts/props/egg_volcanic_lukki_trap.lua
+++ files/scripts/props/egg_volcanic_lukki_trap.lua
@@ -11,31 +11,33 @@ local targets = EntityGetInRadiusWithTag( pos_x, pos_y, r, "player_unit" ) or {}
 for k=1, #targets
 do local target = targets[k];
     if not IsInvisible(target) then
-	local temp_vel = 0
-	local vel_x, vel_y = GameGetVelocityCompVelocity(target)
-	vel_x = math.abs(vel_x)
-	vel_y = math.abs(vel_y)
-	if (vel_y / 3 > vel_x) then
-	    max_vel = vel_y / 3
-	else 
-	    max_vel = vel_x
-	end
-	if (temp_vel > max_vel) then
-	    max_vel = temp_vel
-	end
+        local temp_vel = 0
+        local vel_x, vel_y = GameGetVelocityCompVelocity(target)
+        vel_x = math.abs(vel_x)
+        vel_y = math.abs(vel_y)
+        if (vel_y / 3 > vel_x) then
+            max_vel = vel_y / 3
+        else 
+            max_vel = vel_x
+        end
+        if (temp_vel > max_vel) then
+            max_vel = temp_vel
+        end
     end
 end
 
+--[[
 if (#targets > 0) then
     GamePrint("max_vel:" .. tostring(max_vel))
 end
+]]--
 
 local luacomp = nil
 local luacomps = EntityGetComponentIncludingDisabled(entity_id, "LuaComponent") or {}
 for i = 1, #luacomps do
     if ComponentGetValue2(luacomps[i], "script_source_file") == "mods/Apotheosis/files/scripts/props/egg_volcanic_lukki_trap_shift.lua" then
-	luacomp = luacomps[i]
-	break
+	    luacomp = luacomps[i]
+	    break
     end
 end
 
@@ -45,7 +47,7 @@ elseif (max_vel == 0) then
     local cooldown = Random(100,190)
     ComponentSetValue2( luacomp, "execute_every_n_frame", cooldown )
 else
-    local limit = 0.94
+    local limit = 0.90
     local fastest = 10
     local slowest = 180 - fastest
     local divider = math.max((limit - max_vel) / limit, 0)
@@ -53,7 +55,7 @@ else
 
     ComponentSetValue2( luacomp, "execute_every_n_frame", cooldown )
     if (divider == 0) then
-	ComponentSetValue2( luacomp, "mNextExecutionTime", GameGetFrameNum() + fastest )
+	    ComponentSetValue2( luacomp, "mNextExecutionTime", GameGetFrameNum() + fastest )
     end
 end
 
@@ -63,11 +65,11 @@ local caution = ComponentGetValue2(varcomp, "value_float")
 if (hatch == true or caution > 4) then
     EntityLoad( "data/entities/animals/lukki_fire_tiny.xml", pos_x, pos_y )
     if ( Random( 1, 8 ) == 8 ) then
-	EntityLoad( "data/entities/animals/lukki_fire_tiny.xml", pos_x, pos_y )
+	    EntityLoad( "data/entities/animals/lukki_fire_tiny.xml", pos_x, pos_y )
     end
     EntityLoad( "mods/Apotheosis/files/entities/projectiles/fireball_explosion.xml", pos_x, pos_y )
     EntityKill( entity_id )
 end
 
-local new_caution = math.max(caution - 0.1, 0)
+local new_caution = math.max(caution - 0.05, 0)
 ComponentSetValue2( varcomp, "value_float", new_caution )
