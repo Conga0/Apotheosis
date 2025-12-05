d0dfbbc1f0a8e15691618c8bec2f7a2348d5b76f
Copi <54731850+Ramiels@users.noreply.github.com>
2025-12-05 04:16:11 -0500

    mana multiplier functionality

--- files/gun.lua
+++ files/gun.lua
@@ -24,6 +24,7 @@ function draw_shot( ... )
 
     if call_end_cast then
         apo_state.new_cast = nil
+		apo_state.mana_multiplier = 1.0
 		apo_state.min_reload = -math.huge
     end
 
@@ -36,7 +37,6 @@ function draw_action(...)
 	local ret = apo_state.old.draw_action(...)
 	-- Force recharge if learning orb
 	if apo_state.min_reload then current_reload_time = math.max(apo_state.min_reload, current_reload_time) end
-
 	return ret
 end
 
@@ -70,6 +70,26 @@ do	--Detect if a spell is being cast from within a trigger, and if so how deep
 		apo_state.trigger_nesting = apo_state.trigger_nesting - 1
 		_EndTrigger(...)
 	end
+end
+
+
+
+
+-- Patch order_deck to allow mana skip/multiply
+local order_deck_old = order_deck
+function order_deck(...)
+	for i=1, #deck do
+		local action = deck[i]
+		if action.mana_calculated==nil then
+			-- store base mana
+			local base_mana = action.mana or 0
+			action.mana = nil
+			-- Multiply the mana cost on indexing the mana cost, or skip it outright
+			setmetatable(action, { __index=function(table, key) if key == "mana" then return action.skip_mana and 0 or (base_mana * apo_state.mana_multiplier) end end })
+			action.mana_calculated = true
+		end
+	end
+	order_deck_old(...)
 end
 
 dofile_once("mods/Apotheosis/files/datat.lua")
