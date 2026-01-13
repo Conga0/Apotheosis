4f7341f6bc9963fa10c8b674380841dd4e6a14ec
UserK <111083281+FatalUserK@users.noreply.github.com>
2026-01-13 16:59:34 +0000

    fixed some stuff up

--- files/scripts/mod_compatibility/parallel_parity_append.lua
+++ files/scripts/mod_compatibility/parallel_parity_append.lua
@@ -1,9 +1,11 @@
 local par = ParallelParity
 
+--this adds your biome map to the list of biome maps we go over for the regular `_pixel_scenes.xml` file
 table.insert(par.worlds["data/biome/_pixel_scenes.xml"], "mods/Apotheosis/files/biome_impl/biome_map.png")
+
 par.settings.apotheosis = {
-    statue_room = ModSettingGet("parallel_parity.apotheosis.statue_room") --this is basically "parallel_parity.mods." .. (path to your setting in settings append)
+    statue_room = ModSettingGet("parallel_parity.mods.apotheosis.statue_room") --this is basically "parallel_parity.mods." .. (path to your setting in settings append)
 }
 
-par.pixel_scenes["mods/Apotheosis/files/pixel_scenes/pride_statues/statue_room.png"] = par.settings.apotheosis.statue_room or true
-par.pixel_scenes["mods/Apotheosis/files/scripts/biomes/boss_spawns/statue_room_populator_entity.xml"] = par.settings.apotheosis.statue_room or true
+par.pixel_scenes["mods/Apotheosis/files/pixel_scenes/pride_statues/statue_room.png"] = par.settings.apotheosis.statue_room
+par.pixel_scenes["mods/Apotheosis/files/scripts/biomes/boss_spawns/statue_room_populator_entity.xml"] = par.settings.apotheosis.statue_room
